3b0556b6c6aaa2da83f27a8aab78db4f41a35b77
witnessivan <vania.cool-ivan@yandex.ru>
2019-10-05 10:52:31 +0300

    first commit

---
+++ factorial_1.lisp
@@ -0,0 +1,12 @@
+(defun factorial(n)
+    (if (= n 0)
+        1
+        (* n (factorial (- n 1)))))
+
+(loop for i from 0 to 16
+    do (format t "~D! = ~D~%" i (factorial i)))
+(loop for i from 0 to 16
+    do (format t "~D," (factorial i))
+    finally (format t "..~%"))
+
+  
